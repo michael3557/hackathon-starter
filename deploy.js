c33105a82d1dfe9d0f3c18f436616723a445c948
Michael Davis <michaeld3557@gmail.com>
2019-02-18 08:10:52 -0700

    changed deploy.js to fix error with host ip changing

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '3.82.225.150',
+      host: '3.87.198.45',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
