6b1a8250620b7348e91c5755c7b9c01dd4b40512
Michael Davis <michaeld3557@gmail.com>
2019-02-21 12:43:25 -0700

    changed re-key.pem in deploy.j

--- deploy.js
+++ deploy.js
@@ -87,7 +87,7 @@ function sshConnect() {
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
       host: '3.82.216.26',
       username: 'ubuntu',
-      privateKey: 'hs-key.pem'
+      privateKey: 're-key.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
