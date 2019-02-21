c620f8730f8812401b4b297a8d67233de8ca6356
Michael Davis <michaeld3557@gmail.com>
2019-02-21 12:37:45 -0700

    changed server ip address

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '3.87.198.45',
+      host: '3.82.216.26',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
